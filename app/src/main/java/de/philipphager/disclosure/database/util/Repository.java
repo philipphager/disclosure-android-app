441c26137feb55d91cc0163f70c7a15c732f89fe
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-12 11:31:24 +0200

    Add interfaces for a repository pattern
    Altered version of this pattern:
    https://medium.com/@krzychukosobudzki/repository-design-pattern-bc490b25
    6006

---
+++ app/src/main/java/de/philipphager/disclosure/database/util/Repository.java
@@ -0,0 +1,16 @@
+package de.philipphager.disclosure.database.util;
+
+import java.util.List;
+import rx.Observable;
+
+public interface Repository<T> {
+  void add(T item);
+
+  void add(Iterable<T> items);
+
+  void update(T item);
+
+  void remove(T item);
+
+  Observable<List<T>> query(BriteQuery<T> briteQuery);
+}
