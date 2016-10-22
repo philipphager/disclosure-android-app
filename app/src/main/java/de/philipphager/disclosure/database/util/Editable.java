3d451c55e10a82ccff4caec252362033594a66da
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-23 01:17:19 +0200

    Separate Repository interface into read / write parts

--- app/src/main/java/de/philipphager/disclosure/database/util/Editable.java
+++ app/src/main/java/de/philipphager/disclosure/database/util/Editable.java
@@ -1,4 +1,13 @@
 package de.philipphager.disclosure.database.util;
 
-public interface Editable {
+import android.database.sqlite.SQLiteDatabase;
+
+public interface Editable<T> {
+  long add(SQLiteDatabase db, T item);
+
+  void add(SQLiteDatabase db, Iterable<T> items);
+
+  void update(SQLiteDatabase db, T item);
+
+  void remove(SQLiteDatabase db, T item);
 }
--- app/src/main/java/de/philipphager/disclosure/database/util/Repository.java
+++ app/src/main/java/de/philipphager/disclosure/database/util/Repository.java
@@ -1,16 +1,5 @@
 package de.philipphager.disclosure.database.util;
 
-import java.util.List;
-import rx.Observable;
+public interface Repository<T> extends Editable<T>, Queryable<T> {
 
-public interface Repository<T> {
-  long add(T item);
-
-  void add(Iterable<T> items);
-
-  void update(T item);
-
-  void remove(T item);
-
-  Observable<List<T>> query(BriteQuery<T> briteQuery);
 }
