412408f484ac2eb02f803c6360e03e2a37b8a6f3
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-22 16:21:17 +0200

    DB migration 4, VersionMigration
    Add new version model to db.

--- app/src/main/java/de/philipphager/disclosure/database/DatabaseModule.java
+++ app/src/main/java/de/philipphager/disclosure/database/DatabaseModule.java
@@ -5,9 +5,15 @@ import android.util.SparseArray;
 import com.squareup.sqlbrite.SqlBrite;
 import dagger.Module;
 import dagger.Provides;
+import de.philipphager.disclosure.database.app.AppRepository;
+import de.philipphager.disclosure.database.app.model.App;
 import de.philipphager.disclosure.database.migration.Migration;
 import de.philipphager.disclosure.database.migration.Migrator;
 import de.philipphager.disclosure.database.migration.version.AddAppVersionMigration;
+import de.philipphager.disclosure.database.migration.version.AddVersionNameMigration;
+import de.philipphager.disclosure.database.util.Repository;
+import de.philipphager.disclosure.database.version.VersionRepository;
+import de.philipphager.disclosure.database.version.model.Version;
 import javax.inject.Singleton;
 
 @Module public class DatabaseModule {
@@ -19,10 +25,19 @@ import javax.inject.Singleton;
   @Provides @Singleton public Migrator getMigrator() {
     SparseArray<Class<? extends Migration>> migrations = new SparseArray<>();
     migrations.put(3, AddAppVersionMigration.class);
+    migrations.put(4, AddVersionNameMigration.class);
     return new Migrator(migrations);
   }
 
   @Provides @Singleton public SqlBrite getSqlBrite() {
     return SqlBrite.create();
+  }
+
+  @Provides public Repository<App> getAppRepository(DatabaseManager databaseManager) {
+    return new AppRepository(databaseManager);
+  }
+
+  @Provides public Repository<Version> getVersionRepository(DatabaseManager databaseManager) {
+    return new VersionRepository(databaseManager);
   }
 }
