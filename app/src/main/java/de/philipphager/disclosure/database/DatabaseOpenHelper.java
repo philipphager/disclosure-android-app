b316337585635c7055b96d6e547f91969f1c4701
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-22 16:22:22 +0200

    WIP, ignore further migrations

--- app/src/main/java/de/philipphager/disclosure/database/DatabaseOpenHelper.java
+++ app/src/main/java/de/philipphager/disclosure/database/DatabaseOpenHelper.java
@@ -3,12 +3,14 @@ package de.philipphager.disclosure.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import de.philipphager.disclosure.database.app.model.App;
 import de.philipphager.disclosure.database.migration.Migrator;
+import de.philipphager.disclosure.database.version.model.Version;
 import javax.inject.Inject;
 
 public class DatabaseOpenHelper extends SQLiteOpenHelper {
   private static final String DB_NAME = "disclosure.db";
-  private static final int DB_VERSION = 3;
+  private static final int DB_VERSION = 4;
   private final Migrator migrator;
 
   @Inject public DatabaseOpenHelper(Context context, Migrator migrator) {
@@ -18,9 +20,11 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
 
   @Override public void onCreate(SQLiteDatabase db) {
     //Initial SQL schema creation
+    db.execSQL(App.CREATE_TABLE);
+    db.execSQL(Version.CREATE_TABLE);
   }
 
   @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    migrator.migrate(db, oldVersion, newVersion);
+    //migrator.migrate(db, oldVersion, newVersion);
   }
 }
