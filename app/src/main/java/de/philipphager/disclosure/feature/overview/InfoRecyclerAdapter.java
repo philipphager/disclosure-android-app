806cd230e8d8500fc5b6a8805e655b0d067f2878
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-24 22:21:10 +0200

    Fix grammar in comment

--- app/src/main/java/de/philipphager/disclosure/feature/overview/InfoRecyclerAdapter.java
+++ app/src/main/java/de/philipphager/disclosure/feature/overview/InfoRecyclerAdapter.java
@@ -1,4 +1,4 @@
-package de.philipphager.disclosure.feature.list;
+package de.philipphager.disclosure.feature.overview;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
