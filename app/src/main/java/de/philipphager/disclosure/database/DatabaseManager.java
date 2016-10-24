fe9a36f83296c0243f6f78bda38b0c128e7dd2a9
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-24 17:23:20 +0200

    Remove usages of SQLiteDatabase
    Replaced write database access with BriteDatabase
    to trigger a database update.
    Tis unifies the app to only use one database accessor.

--- app/src/main/java/de/philipphager/disclosure/database/DatabaseManager.java
+++ app/src/main/java/de/philipphager/disclosure/database/DatabaseManager.java
@@ -1,6 +1,5 @@
 package de.philipphager.disclosure.database;
 
-import android.database.sqlite.SQLiteDatabase;
 import com.squareup.sqlbrite.BriteDatabase;
 import com.squareup.sqlbrite.SqlBrite;
 import javax.inject.Inject;
@@ -10,46 +9,18 @@ public class DatabaseManager {
   private final DatabaseOpenHelper openHelper;
   private final SqlBrite sqlBrite;
   private BriteDatabase observableDB;
-  private SQLiteDatabase writeableDB;
 
   @Inject public DatabaseManager(DatabaseOpenHelper openHelper, SqlBrite sqlBrite) {
     this.openHelper = openHelper;
     this.sqlBrite = sqlBrite;
   }
 
-  public SQLiteDatabase openWriteable() {
-    synchronized (this) {
-      if (writeableDB == null) {
-        writeableDB = openHelper.getWritableDatabase();
-      }
-      return writeableDB;
-    }
-  }
-
-  public void closeWriteable() {
-    synchronized (this) {
-      if (writeableDB != null) {
-        writeableDB.close();
-        writeableDB = null;
-      }
-    }
-  }
-
-  public BriteDatabase openObservable() {
+  public BriteDatabase get() {
     synchronized (this) {
       if (observableDB == null) {
         observableDB = sqlBrite.wrapDatabaseHelper(openHelper, Schedulers.io());
       }
       return observableDB;
-    }
-  }
-
-  public void closeObservable() {
-    synchronized (this) {
-      if (observableDB != null) {
-        observableDB.close();
-        observableDB = null;
-      }
     }
   }
 }
