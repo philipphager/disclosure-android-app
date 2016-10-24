a419e3273f72ba54ecc9dd4553205e2fb550bd00
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-24 21:01:06 +0200

    Add a HomeActivity with bottom navigation
    Starting Activity for the app.
    Manages Fragments in a bottom navigation.

--- app/src/main/java/de/philipphager/disclosure/ApplicationComponent.java
+++ app/src/main/java/de/philipphager/disclosure/ApplicationComponent.java
@@ -3,11 +3,17 @@ package de.philipphager.disclosure;
 import dagger.Component;
 import de.philipphager.disclosure.database.DatabaseModule;
 import de.philipphager.disclosure.feature.device.AndroidModule;
+import de.philipphager.disclosure.feature.home.HomeActivity;
 import de.philipphager.disclosure.feature.list.AppListActivity;
+import de.philipphager.disclosure.feature.list.AppOverviewFragment;
 import javax.inject.Singleton;
 
 @Singleton
 @Component(modules = {ApplicationModule.class, DatabaseModule.class, AndroidModule.class})
 public interface ApplicationComponent {
+  void inject(HomeActivity activity);
+
+  void inject(AppOverviewFragment fragment);
+
   void inject(AppListActivity activity);
 }
---
+++ app/src/main/java/de/philipphager/disclosure/feature/home/HomeActivity.java
@@ -0,0 +1,45 @@
+package de.philipphager.disclosure.feature.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import butterknife.BindView;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
+import de.philipphager.disclosure.ApplicationComponent;
+import de.philipphager.disclosure.R;
+import de.philipphager.disclosure.util.ui.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+  private static final boolean SMOOTH_SCROLL_ENABLED = true;
+
+  @BindView(R.id.view_pager) ViewPager viewPager;
+  @BindView(R.id.bottom_navigation) AHBottomNavigation bottomNavigation;
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+
+    setupViewPager();
+    setupBottomNavigation();
+  }
+
+  @Override protected void injectActivity(ApplicationComponent appComponent) {
+    appComponent.inject(this);
+  }
+
+  private void setupViewPager() {
+    HomeAdapter homeAdapter = new HomeAdapter(getSupportFragmentManager());
+    viewPager.setAdapter(homeAdapter);
+  }
+
+  private void setupBottomNavigation() {
+    AHBottomNavigationAdapter adapter = new AHBottomNavigationAdapter(this, R.menu.navigation_home);
+    adapter.setupWithBottomNavigation(bottomNavigation, null);
+
+    bottomNavigation.setOnTabSelectedListener((position, wasSelected) -> {
+      viewPager.setCurrentItem(position, SMOOTH_SCROLL_ENABLED);
+      return true;
+    });
+  }
+}
