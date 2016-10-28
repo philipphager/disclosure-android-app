11ec0d9c915f39702469057c0953cbf3e9db86ed
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-10-28 14:36:51 +0200

    Rename AndroidModule

--- app/src/main/java/de/philipphager/disclosure/ApplicationComponent.java
+++ app/src/main/java/de/philipphager/disclosure/ApplicationComponent.java
@@ -2,13 +2,13 @@ package de.philipphager.disclosure;
 
 import dagger.Component;
 import de.philipphager.disclosure.database.DatabaseModule;
-import de.philipphager.disclosure.feature.device.AndroidModule;
+import de.philipphager.disclosure.feature.device.DeviceModule;
 import de.philipphager.disclosure.feature.home.HomeActivity;
 import de.philipphager.disclosure.feature.overview.AppOverviewFragment;
 import javax.inject.Singleton;
 
 @Singleton
-@Component(modules = {ApplicationModule.class, DatabaseModule.class, AndroidModule.class})
+@Component(modules = {ApplicationModule.class, DatabaseModule.class, DeviceModule.class})
 public interface ApplicationComponent {
   void inject(HomeActivity activity);
 
