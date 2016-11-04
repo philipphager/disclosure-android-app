06a5b3f8693cde84dad3cd0f82973e7e11d8f62a
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-11-04 13:21:55 +0100

    Refactor tests

--- app/src/test/java/de/philipphager/disclosure/service/AppServiceShould.java
+++ app/src/test/java/de/philipphager/disclosure/service/AppServiceShould.java
@@ -2,15 +2,11 @@ package de.philipphager.disclosure.service;
 
 import com.squareup.sqlbrite.BriteDatabase;
 import de.philipphager.disclosure.database.DatabaseManager;
+import de.philipphager.disclosure.database.app.AppRepository;
 import de.philipphager.disclosure.database.app.MockApp;
 import de.philipphager.disclosure.database.app.mapper.ToAppMapper;
 import de.philipphager.disclosure.database.app.model.App;
-import de.philipphager.disclosure.database.app.query.QueryAllApps;
-import de.philipphager.disclosure.database.app.query.QueryAppsByName;
-import de.philipphager.disclosure.database.info.query.QueryAllAppInfos;
-import de.philipphager.disclosure.database.util.repository.Queryable;
-import de.philipphager.disclosure.database.util.repository.Repository;
-import de.philipphager.disclosure.database.version.model.Version;
+import de.philipphager.disclosure.database.version.VersionRepository;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -33,9 +29,8 @@ import static org.mockito.Mockito.when;
 @PrepareForTest({BriteDatabase.class})
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public class AppServiceShould {
-  @Mock protected Repository<App> appRepository;
-  @Mock protected Queryable<App.Info> appInfoRepository;
-  @Mock protected Repository<Version> versionRepository;
+  @Mock protected AppRepository appRepository;
+  @Mock protected VersionRepository versionRepository;
   @Mock protected DatabaseManager databaseManager;
   @Mock protected ToAppMapper toAppMapper;
   private AppService appService;
@@ -46,14 +41,13 @@ public class AppServiceShould {
     BriteDatabase database = PowerMockito.mock(BriteDatabase.class);
     when(databaseManager.get()).thenReturn(database).thenReturn(null);
 
-    appService = new AppService(appRepository, appInfoRepository,
-        versionRepository, databaseManager, toAppMapper);
+    appService = new AppService(databaseManager, appRepository, versionRepository, toAppMapper);
   }
 
   @Test @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
   public void fetchAllApps() {
     List<App> testApps = Arrays.asList(MockApp.TEST, MockApp.TEST2);
-    when(appRepository.query(any(BriteDatabase.class), any(QueryAllApps.class)))
+    when(appRepository.all(any(BriteDatabase.class)))
         .thenReturn(Observable.just(testApps));
 
     TestSubscriber<List<App>> subscriber = new TestSubscriber<>();
@@ -68,31 +62,14 @@ public class AppServiceShould {
   }
 
   @Test @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
-  public void fetchAllAppsByName() {
-    List<App> testApps = Arrays.asList(MockApp.TEST2);
-    when(appRepository.query(any(BriteDatabase.class), any(QueryAppsByName.class)))
-        .thenReturn(Observable.just(testApps));
-
-    TestSubscriber<List<App>> subscriber = new TestSubscriber<>();
-
-    appService.byName("test")
-        .toBlocking()
-        .subscribe(subscriber);
-
-    verify(databaseManager).get();
-    subscriber.assertReceivedOnNext(Collections.singletonList(testApps));
-    subscriber.assertCompleted();
-  }
-
-  @Test @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
   public void fetchAllAppInfos() {
     List<App.Info> testInfos = Arrays.asList(MockApp.TEST_INFO, MockApp.TEST2_INFO);
-    when(appInfoRepository.query(any(BriteDatabase.class), any(QueryAllAppInfos.class)))
+    when(appRepository.allInfos(any(BriteDatabase.class)))
         .thenReturn(Observable.just(testInfos));
 
     TestSubscriber<List<App.Info>> subscriber = new TestSubscriber<>();
 
-    appService.allAppInfos()
+    appService.allInfos()
         .toBlocking()
         .subscribe(subscriber);
 
