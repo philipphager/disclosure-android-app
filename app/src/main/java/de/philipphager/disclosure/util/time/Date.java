75232fb765ca7dfb2c0b2e3ef123e210757cfdaa
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-11-07 23:52:48 +0100

    Date.MIN should be final

--- app/src/main/java/de/philipphager/disclosure/util/time/Date.java
+++ app/src/main/java/de/philipphager/disclosure/util/time/Date.java
@@ -4,7 +4,7 @@ import org.threeten.bp.OffsetDateTime;
 import org.threeten.bp.ZoneOffset;
 
 public final class Date {
-  public static OffsetDateTime MIN = OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+  public static final OffsetDateTime MIN = OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
 
   private Date() {
     // No instances.
