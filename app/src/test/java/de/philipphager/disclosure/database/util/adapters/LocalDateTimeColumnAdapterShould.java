68010983ce5388d3dedea6999615a984f41a0da7
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-11-13 22:43:36 +0100

    Fix LocalDateTime in tests

--- app/src/test/java/de/philipphager/disclosure/database/util/adapters/LocalDateTimeColumnAdapterShould.java
+++ app/src/test/java/de/philipphager/disclosure/database/util/adapters/LocalDateTimeColumnAdapterShould.java
@@ -13,7 +13,7 @@ import static org.threeten.bp.temporal.ChronoUnit.MILLIS;
   @InjectMocks protected LocalDateTimeColumnAdapter adapter;
 
   @Test public void encodeMillisToStartOfAtUtcStandard() {
-    LocalDateTime utcStartOfTime = LocalDateTime.of(1970, 1, 1, 1, 0, 0, 0);
+    LocalDateTime utcStartOfTime = LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0);
 
     Long millis = adapter.encode(utcStartOfTime);
 
@@ -21,7 +21,7 @@ import static org.threeten.bp.temporal.ChronoUnit.MILLIS;
   }
 
   @Test public void encodeMillisWithCorrectDuration() {
-    LocalDateTime start = LocalDateTime.of(1970, 1, 1, 1, 0, 0, 0);
+    LocalDateTime start = LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0);
     LocalDateTime end = LocalDateTime.of(2016, 1, 1, 1, 0, 0, 0);
     long duration = start.until(end, MILLIS);
 
@@ -31,14 +31,14 @@ import static org.threeten.bp.temporal.ChronoUnit.MILLIS;
   }
 
   @Test public void decodeDateFromMillisStartCorrectly() {
-    LocalDateTime utcStartOfTime = LocalDateTime.of(1970, 1, 1, 1, 0, 0, 0);
+    LocalDateTime utcStartOfTime = LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0);
     LocalDateTime date = adapter.decode(0L);
 
     assertThat(date).isEqualTo(utcStartOfTime);
   }
 
   @Test public void decodeDateFromMillisDurationCorrectly() {
-    LocalDateTime start = LocalDateTime.of(1970, 1, 1, 1, 0, 0, 0);
+    LocalDateTime start = LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0);
     LocalDateTime end = LocalDateTime.of(2016, 1, 1, 1, 0, 0, 0);
 
     long duration = start.until(end, MILLIS);
