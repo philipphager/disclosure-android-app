2c459442748114ba393531523f15468b71f764da
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-11-13 21:59:15 +0100

    Add mock models for testing

--- app/src/test/java/de/philipphager/disclosure/database/mocks/MockPackageInfo.java
+++ app/src/test/java/de/philipphager/disclosure/database/mocks/MockPackageInfo.java
@@ -11,11 +11,13 @@ public final class MockPackageInfo {
   public static final PackageInfo TEST = new PackageInfo() {{
     packageName = MockApp.TEST.packageName();
     versionCode = MockApp.TEST_INFO.versionCode();
+    versionName = String.valueOf(MockApp.TEST_INFO.versionCode());
   }};
 
   @SuppressWarnings("PMD.NonStaticInitializer")
   public static final PackageInfo TEST2 = new PackageInfo() {{
     packageName = MockApp.TEST2.packageName();
     versionCode = MockApp.TEST2_INFO.versionCode();
+    versionName = String.valueOf(MockApp.TEST2_INFO.versionCode());
   }};
 }
