b9e09e98dc02841850cd473ab0883394d630244a
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-12-26 15:11:49 +0100

    Add edit / uninstall buttons to app detail activity

--- app/src/main/java/de/philipphager/disclosure/feature/app/detail/DetailActivity.java
+++ app/src/main/java/de/philipphager/disclosure/feature/app/detail/DetailActivity.java
@@ -2,20 +2,27 @@ package de.philipphager.disclosure.feature.app.detail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.OnClick;
+import butterknife.OnLongClick;
 import de.philipphager.disclosure.ApplicationComponent;
 import de.philipphager.disclosure.R;
 import de.philipphager.disclosure.database.app.model.App;
 import de.philipphager.disclosure.database.library.model.Library;
+import de.philipphager.disclosure.feature.app.detail.tutorials.EditPermissionsTutorialDialog;
+import de.philipphager.disclosure.feature.app.detail.tutorials.RuntimePermissionsTutorialDialog;
 import de.philipphager.disclosure.util.ui.BaseActivity;
 import de.philipphager.disclosure.util.ui.components.ScoreView;
 import de.philipphager.disclosure.util.ui.image.AppIconLoader;
@@ -27,11 +34,14 @@ import static de.philipphager.disclosure.util.assertion.Assertions.ensureNotNull
 
 public class DetailActivity extends BaseActivity implements DetailView {
   private static final String EXTRA_APP = "EXTRA_APP";
+  private static final String EDIT_PERMISSIONS_DIALOG = "EDIT_PERMISSIONS_DIALOG";
+  private static final String PERMISSIONS_UNSUPPORTED_DIALOG = "PERMISSIONS_UNSUPPORTED_DIALOG";
 
   @BindView(R.id.icon) protected ImageView icon;
   @BindView(R.id.activity_detail) protected View view;
   @BindView(R.id.scoreView) protected ScoreView scoreView;
   @BindView(R.id.app_detail_libraries) protected RecyclerView libraryListRecyclerView;
+  @BindView(R.id.btn_edit_settings) protected Button btnEditSettings;
   @Inject protected DetailPresenter presenter;
   private LibraryRecyclerAdapter adapter;
 
@@ -69,6 +79,12 @@ public class DetailActivity extends BaseActivity implements DetailView {
     super.onDestroy();
   }
 
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    presenter.onActivityResult(requestCode, resultCode);
+  }
+
   @Override protected void injectActivity(ApplicationComponent appComponent) {
     appComponent.inject(this);
   }
@@ -94,11 +110,42 @@ public class DetailActivity extends BaseActivity implements DetailView {
     Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
   }
 
+  @Override public void showEditPermissionsTutorial(String packageName) {
+    EditPermissionsTutorialDialog.newInstance(packageName)
+        .show(getSupportFragmentManager(), EDIT_PERMISSIONS_DIALOG);
+  }
+
+  @Override public void showRuntimePermissionsTutorial(String packageName) {
+    RuntimePermissionsTutorialDialog.newInstance()
+        .show(getSupportFragmentManager(), PERMISSIONS_UNSUPPORTED_DIALOG);
+  }
+
+  @Override public void enableEditPermissions(boolean isEnabled) {
+    int id = isEnabled ? R.drawable.ic_mode_edit : R.drawable.ic_mode_edit_disabled;
+    int text = isEnabled ? R.color.colorPrimary : R.color.background_disabled;
+
+    Drawable editIcon = ResourcesCompat.getDrawable(getResources(), id, null);
+    btnEditSettings.setCompoundDrawablesWithIntrinsicBounds(null, editIcon, null, null);
+    btnEditSettings.setTextColor(ContextCompat.getColor(this, text));
+  }
+
   @Override public void setScore(ScoreView.Score score) {
     scoreView.setScore(score);
   }
 
-  @OnClick(R.id.btn_analyse) public void onAnalyseButtonClick() {
-    presenter.onAnalyseButtonClick();
+  @OnClick(R.id.btn_analyse) public void onAnalyseButtonClicked() {
+    presenter.onAnalyseClicked();
+  }
+
+  @OnClick(R.id.btn_edit_settings) public void onEditSettingsClicked() {
+    presenter.onEditPermissionsClicked();
+  }
+
+  @OnLongClick(R.id.btn_edit_settings) public boolean onEditSettingsLongClicked() {
+    return presenter.onEditPermissionsLongClicked();
+  }
+
+  @OnClick(R.id.btn_uninstall) public void onUninstallClicked() {
+    presenter.onUninstallClicked();
   }
 }
