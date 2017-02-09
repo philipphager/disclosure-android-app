d3414b854351dc7020369f75d9a4d829dab99805
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-02-10 00:06:51 +0100

    Minor housekeeping

--- app/src/main/java/de/philipphager/disclosure/util/device/InternalStorageProvider.java
+++ app/src/main/java/de/philipphager/disclosure/util/device/InternalStorageProvider.java
@@ -3,11 +3,12 @@ package de.philipphager.disclosure.util.device;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import java.io.File;
+import javax.inject.Inject;
 
 public class InternalStorageProvider implements StorageProvider {
   private final Context context;
 
-  public InternalStorageProvider(Context context) {
+  @Inject public InternalStorageProvider(Context context) {
     this.context = context;
   }
 
