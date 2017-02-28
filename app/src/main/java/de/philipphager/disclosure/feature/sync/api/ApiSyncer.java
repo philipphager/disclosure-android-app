d61089a195614db22af7d07a11b74422bbdcac44
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-02-28 01:41:15 +0100

    Fix api endpoint and remove library features

--- app/src/main/java/de/philipphager/disclosure/feature/sync/api/ApiSyncer.java
+++ app/src/main/java/de/philipphager/disclosure/feature/sync/api/ApiSyncer.java
@@ -1,8 +1,7 @@
 package de.philipphager.disclosure.feature.sync.api;
 
-import de.philipphager.disclosure.feature.sync.api.usecases.SyncFeatures;
+import de.philipphager.disclosure.database.library.model.Library;
 import de.philipphager.disclosure.feature.sync.api.usecases.SyncLibraries;
-import de.philipphager.disclosure.feature.sync.api.usecases.SyncLibraryFeatures;
 import de.philipphager.disclosure.util.time.Stopwatch;
 import java.util.List;
 import javax.inject.Inject;
@@ -11,32 +10,24 @@ import timber.log.Timber;
 
 public class ApiSyncer {
   private final SyncLibraries syncLibraries;
-  private final SyncFeatures syncFeatures;
-  private final SyncLibraryFeatures syncLibraryFeatures;
   private final Stopwatch stopwatch;
 
   @Inject public ApiSyncer(SyncLibraries syncLibraries,
-      SyncFeatures syncFeatures,
-      SyncLibraryFeatures syncLibraryFeatures,
       Stopwatch stopwatch) {
     this.syncLibraries = syncLibraries;
-    this.syncFeatures = syncFeatures;
-    this.syncLibraryFeatures = syncLibraryFeatures;
     this.stopwatch = stopwatch;
   }
 
-  public Observable<List<?>> sync() {
+  public Observable<List<Library>> sync() {
     stopwatch.start();
 
-    return Observable.concat(
-        Observable.concatEager(syncLibraries.run(), syncFeatures.run()),
-        syncLibraryFeatures.run()
-    ).doOnError(throwable -> {
-      stopwatch.stop();
-      Timber.e(throwable, "api sync failed, %s ", stopwatch);
-    }).doOnCompleted(() -> {
-      stopwatch.stop();
-      Timber.d("api sync finished, %s ", stopwatch);
-    }).ignoreElements();
+    return syncLibraries.run()
+        .doOnError(throwable -> {
+          stopwatch.stop();
+          Timber.e(throwable, "api sync failed, %s ", stopwatch);
+        }).doOnCompleted(() -> {
+          stopwatch.stop();
+          Timber.d("api sync finished, %s ", stopwatch);
+        }).ignoreElements();
   }
 }
