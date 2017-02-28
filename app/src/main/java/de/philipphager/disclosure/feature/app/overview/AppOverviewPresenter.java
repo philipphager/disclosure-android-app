237569f2bf59596996571deb455f0ea59f5ddacb
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-02-28 02:07:15 +0100

    Move app analysis into HomePresenter
    Analysis can only take place after Home api / device sync

--- app/src/main/java/de/philipphager/disclosure/feature/app/overview/AppOverviewPresenter.java
+++ app/src/main/java/de/philipphager/disclosure/feature/app/overview/AppOverviewPresenter.java
@@ -3,50 +3,23 @@ package de.philipphager.disclosure.feature.app.overview;
 import android.view.MenuItem;
 import com.f2prateek.rx.preferences.Preference;
 import de.philipphager.disclosure.R;
-import de.philipphager.disclosure.feature.analyser.app.usecase.AnalyseApps;
 import de.philipphager.disclosure.feature.preference.ui.AppListSortBy;
-import de.philipphager.disclosure.service.app.AppService;
 import de.philipphager.disclosure.service.app.filter.SortBy;
 import javax.inject.Inject;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.subscriptions.CompositeSubscription;
-import timber.log.Timber;
 
 public class AppOverviewPresenter {
-  private final AppService appService;
-  private final AnalyseApps analyseApps;
   private final Preference<SortBy> sortBy;
-  private CompositeSubscription subscriptions;
   private AppOverviewView view;
 
-  @Inject public AppOverviewPresenter(AppService appService,
-      AnalyseApps analyseApps,
-      @AppListSortBy Preference<SortBy> sortBy) {
-    this.appService = appService;
-    this.analyseApps = analyseApps;
+  @Inject public AppOverviewPresenter(@AppListSortBy Preference<SortBy> sortBy) {
     this.sortBy = sortBy;
   }
 
   public void onViewCreated(AppOverviewView view) {
     this.view = view;
-    this.subscriptions = new CompositeSubscription();
-    analyseAppLibraries();
   }
 
   public void onDestroyView() {
-    subscriptions.unsubscribe();
-  }
-
-  private void analyseAppLibraries() {
-    subscriptions.add(appService.all()
-            .first()
-            .flatMap(analyseApps::analyse)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(libraries -> {
-              Timber.d("App analysis finished");
-            }, throwable -> {
-              Timber.e(throwable, "while analysing all apps");
-            }));
   }
 
   public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/de/philipphager/disclosure/feature/home/HomePresenter.java
+++ app/src/main/java/de/philipphager/disclosure/feature/home/HomePresenter.java
@@ -1,8 +1,13 @@
 package de.philipphager.disclosure.feature.home;
 
+import de.philipphager.disclosure.database.library.model.Library;
+import de.philipphager.disclosure.feature.analyser.app.usecase.AnalyseApps;
 import de.philipphager.disclosure.feature.sync.api.ApiSyncer;
 import de.philipphager.disclosure.feature.sync.db.DBSyncer;
+import de.philipphager.disclosure.service.app.AppService;
+import java.util.List;
 import javax.inject.Inject;
+import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
@@ -11,47 +16,64 @@ import timber.log.Timber;
 public class HomePresenter {
   private final DBSyncer dbSyncer;
   private final ApiSyncer apiSyncer;
+  private final AnalyseApps analyseApps;
+  private final AppService appService;
   private CompositeSubscription subscriptions;
-  private HomeView homeView;
+  private HomeView view;
 
-  @Inject public HomePresenter(DBSyncer dbSyncer, ApiSyncer apiSyncer) {
+  @Inject public HomePresenter(DBSyncer dbSyncer,
+      ApiSyncer apiSyncer,
+      AnalyseApps analyseApps,
+      AppService appService) {
     this.dbSyncer = dbSyncer;
     this.apiSyncer = apiSyncer;
+    this.analyseApps = analyseApps;
+    this.appService = appService;
   }
 
   public void onCreate(HomeView homeView) {
-    this.homeView = homeView;
+    this.view = homeView;
     this.subscriptions = new CompositeSubscription();
 
-    syncWithApi();
-    syncDBWithDevice();
+    sync();
   }
 
   public void onDestroy() {
     this.subscriptions.clear();
   }
 
-  private void syncWithApi() {
-    subscriptions.add(apiSyncer.sync()
+  private void sync() {
+    subscriptions.add(Observable.concat(
+        Observable.merge(
+            syncDBWithDevice(),
+            syncWithServerApi()),
+        analyzeUsedLibrariesAndPermissions())
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(libraries -> {}, Timber::e));
+        .doOnCompleted(() -> Timber.d("sync completed"))
+        .subscribe(result -> {
+        }, throwable -> {
+          Timber.e(throwable, "while syncing with device and api");
+        }));
   }
 
-  private void syncDBWithDevice() {
-    subscriptions.add(dbSyncer.sync()
-        .subscribe(i -> {
+  private Observable<Integer> syncDBWithDevice() {
+    return dbSyncer.sync();
+  }
 
-        }, throwable -> {
-          Timber.e(throwable, "while syncing apps with the device.");
-        }, () -> {
-          Timber.d("database sync completed.");
-        }));
+  private Observable<List<Library>> syncWithServerApi() {
+    return apiSyncer.sync();
+  }
+
+  private Observable<?> analyzeUsedLibrariesAndPermissions() {
+    return appService.all()
+        .first()
+        .flatMap(analyseApps::analyse);
   }
 
   public boolean onTabSelected(int position, boolean wasSelected) {
     if (!wasSelected) {
-      homeView.setCurrentTab(position);
+      view.setCurrentTab(position);
     }
     return true;
   }
