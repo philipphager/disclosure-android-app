47838c901545e3d60b284208158bf0ebc990fdf2
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-03-03 08:32:59 +0100

    Fix sorting

--- app/src/main/java/de/philipphager/disclosure/feature/app/overview/AppOverviewPresenter.java
+++ app/src/main/java/de/philipphager/disclosure/feature/app/overview/AppOverviewPresenter.java
@@ -26,6 +26,7 @@ public class AppOverviewPresenter {
         return true;
       case R.id.action_sort_library_count:
         sortBy.set(SortBy.LIBRARY_COUNT);
+        return true;
       case R.id.action_sort_analyzed_at:
         sortBy.set(SortBy.ANALYZED_AT);
         return true;
