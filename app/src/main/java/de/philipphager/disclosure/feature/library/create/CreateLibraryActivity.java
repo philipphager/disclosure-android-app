eb214f3c8fe964ed14e82c34259b8c9e92510594
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-03-13 22:28:59 +0100

    Add a new library locally

--- app/src/main/java/de/philipphager/disclosure/feature/library/create/CreateLibraryActivity.java
+++ app/src/main/java/de/philipphager/disclosure/feature/library/create/CreateLibraryActivity.java
@@ -4,13 +4,38 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.OnClick;
+import butterknife.OnItemSelected;
+import butterknife.OnTextChanged;
 import de.philipphager.disclosure.ApplicationComponent;
 import de.philipphager.disclosure.R;
+import de.philipphager.disclosure.database.library.model.Library;
 import de.philipphager.disclosure.util.ui.BaseActivity;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 
-public class CreateLibraryActivity extends BaseActivity {
-@Inject protected CreateLibraryPresenter presenter;
+public class CreateLibraryActivity extends BaseActivity implements CreateLibraryView {
+  @Inject protected CreateLibraryPresenter presenter;
+  @BindView(R.id.toolbar) Toolbar toolbar;
+  @BindView(R.id.ic_done) ImageView done;
+  @BindView(R.id.library_title) EditText title;
+  @BindView(R.id.library_package_name) EditText packageName;
+  @BindView(R.id.library_website) EditText websiteUrl;
+  @BindView(R.id.library_type) Spinner type;
+  @BindView(R.id.library_layout_title) TextInputLayout titleLayout;
+  @BindView(R.id.library_layout_package_name) TextInputLayout packageNameLayout;
+  @BindView(R.id.library_layout_website) TextInputLayout websiteUrlLayout;
+  private ArrayAdapter<String> typeAdapter;
 
   public static Intent launch(Context context) {
     return new Intent(context, CreateLibraryActivity.class);
@@ -20,10 +45,73 @@ public class CreateLibraryActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_library_create);
 
-    presenter.onCreate();
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_clear);
+
+    typeAdapter =
+        new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, new ArrayList<>(4));
+    type.setAdapter(typeAdapter);
+
+    presenter.onCreate(this);
   }
 
   @Override protected void injectActivity(ApplicationComponent appComponent) {
     appComponent.inject(this);
+  }
+
+  @Override public void showTypes(List<Library.Type> types) {
+    for (Library.Type type : types) {
+      typeAdapter.add(type.name());
+    }
+
+    typeAdapter.notifyDataSetChanged();
+  }
+
+  @Override public void setDoneEnabled(boolean isEnabled) {
+    done.setImageResource(isEnabled ? R.drawable.ic_check : R.drawable.ic_check_disabled);
+  }
+
+  @Override public void showTitleError(boolean isEnabled) {
+    titleLayout.setError(getString(R.string.activity_library_create_title_invalid));
+    titleLayout.setErrorEnabled(isEnabled);
+  }
+
+  @Override public void showPackageNameError(boolean isEnabled) {
+    packageNameLayout.setError(getString(R.string.activity_library_create_package_name_invalid));
+    packageNameLayout.setErrorEnabled(isEnabled);
+  }
+
+  @Override public void showWebsiteUrlError(boolean isEnabled) {
+    websiteUrlLayout.setError(getString(R.string.activity_library_create_url_invalid));
+    websiteUrlLayout.setErrorEnabled(isEnabled);
+  }
+
+  @OnTextChanged(R.id.library_title) public void onTitleChanged(Editable title) {
+    presenter.onTitleChanged(title.toString());
+  }
+
+  @OnTextChanged(R.id.library_package_name) public void onPackageNameChanged(Editable packageName) {
+    presenter.onPackageNameChanged(packageName.toString());
+  }
+
+  @OnTextChanged(R.id.library_website) public void onWebsiteUrlChanged(Editable websiteUrl) {
+    presenter.onWebsiteUrlChanged(websiteUrl.toString());
+  }
+
+  @Override public void showError() {
+    Toast.makeText(this, R.string.activity_library_create_error_duplicate, Toast.LENGTH_LONG)
+        .show();
+  }
+
+  @OnItemSelected(R.id.library_type) public void onTypeSelected(Spinner spinner, int position) {
+    if (position < typeAdapter.getCount()) {
+      String type = typeAdapter.getItem(position);
+      presenter.onTypeSelected(Library.Type.valueOf(type));
+    }
+  }
+
+  @OnClick(R.id.ic_done) public void onDoneClicked() {
+    presenter.onDoneClicked();
   }
 }
--- app/src/main/java/de/philipphager/disclosure/feature/library/create/CreateLibraryPresenter.java
+++ app/src/main/java/de/philipphager/disclosure/feature/library/create/CreateLibraryPresenter.java
@@ -1,13 +1,121 @@
 package de.philipphager.disclosure.feature.library.create;
 
+import android.text.TextUtils;
+import android.webkit.URLUtil;
+import de.philipphager.disclosure.database.library.model.Library;
+import de.philipphager.disclosure.feature.library.create.usecase.AddLibraryLocally;
+import java.util.Arrays;
+import java.util.List;
 import javax.inject.Inject;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 public class CreateLibraryPresenter {
-  @Inject public CreateLibraryPresenter() {
+  private static final List<Library.Type> TYPES = Arrays.asList(
+      Library.Type.ADVERTISEMENT,
+      Library.Type.ANALYTICS,
+      Library.Type.DEVELOPER,
+      Library.Type.SOCIAL
+  );
+  private final AddLibraryLocally addLibraryLocally;
+  private CreateLibraryView view;
+  private String title = "";
+  private String packageName = "";
+  private String websiteUrl = "";
+  private Library.Type type;
 
+  @Inject public CreateLibraryPresenter(AddLibraryLocally addLibraryLocally) {
+    this.addLibraryLocally = addLibraryLocally;
   }
 
-  public void onCreate() {
+  public void onCreate(CreateLibraryView view) {
+    this.view = view;
+    initUi();
+  }
 
+  private void initUi() {
+    view.showTypes(TYPES);
+  }
+
+  public void onDoneClicked() {
+    if (validateAll()) {
+
+      addLibraryLocally.run(title, packageName, websiteUrl, type)
+          .subscribeOn(Schedulers.io())
+          .observeOn(AndroidSchedulers.mainThread())
+          .subscribe(library -> {
+            view.finish();
+          }, throwable -> {
+            Timber.e(throwable, "while adding local library");
+            view.showError();
+          });
+    } else {
+      displayErrors();
+    }
+  }
+
+  public void onTitleChanged(String title) {
+    this.title = title;
+    checkDoneButtonEnabled();
+  }
+
+  public void onPackageNameChanged(String packageName) {
+    this.packageName = packageName;
+    checkDoneButtonEnabled();
+  }
+
+  public void onWebsiteUrlChanged(String websiteUrl) {
+    this.websiteUrl = websiteUrl;
+    checkDoneButtonEnabled();
+  }
+
+  public void onTypeSelected(Library.Type type) {
+    this.type = type;
+  }
+
+  private void displayErrors() {
+    view.showTitleError(!validateTitle());
+    view.showPackageNameError(!validatePackageName());
+    view.showWebsiteUrlError(!validateWebsite());
+  }
+
+  private void resetErrors() {
+    view.showTitleError(false);
+    view.showPackageNameError(false);
+    view.showWebsiteUrlError(false);
+  }
+
+  private void checkDoneButtonEnabled() {
+    boolean allValid = validateAll();
+    view.setDoneEnabled(allValid);
+
+    if (allValid) {
+      resetErrors();
+    }
+  }
+
+  private boolean validateAll() {
+    boolean isValid =
+        validateTitle() && validatePackageName() && validateWebsite() && validateType();
+    view.setDoneEnabled(isValid);
+
+    return isValid;
+  }
+
+  private boolean validateTitle() {
+    return !TextUtils.isEmpty(title);
+  }
+
+  private boolean validatePackageName() {
+    return !TextUtils.isEmpty(packageName) && packageName.contains(".");
+  }
+
+  private boolean validateWebsite() {
+    return TextUtils.isEmpty(websiteUrl) || URLUtil.isValidUrl(websiteUrl);
+  }
+
+  private boolean validateType() {
+    return type != null;
   }
 }
