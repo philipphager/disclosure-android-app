243adb38e3d4f61404f4df14a9a5aa18af8638d9
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-03-16 16:43:23 +0100

    Fix not existing .apks failing

--- app/src/main/java/de/philipphager/disclosure/feature/analyser/app/Apk.java
+++ app/src/main/java/de/philipphager/disclosure/feature/analyser/app/Apk.java
@@ -2,7 +2,9 @@ package de.philipphager.disclosure.feature.analyser.app;
 
 import dalvik.system.DexFile;
 import de.philipphager.disclosure.database.app.model.App;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import rx.Observable;
@@ -19,8 +21,13 @@ public class Apk {
   }
 
   private void load() throws IOException {
-    DexFile dexFile = new DexFile(app.sourceDir());
-    List<String> classNames = Collections.list(dexFile.entries());
+    File file = new File(app.sourceDir());
+    List<String> classNames = new ArrayList<>();
+
+    if (file.exists()) {
+      DexFile dexFile = new DexFile(app.sourceDir());
+      classNames.addAll(Collections.list(dexFile.entries()));
+    }
 
     sortedClassNames = Observable.from(classNames)
         .distinct()
@@ -34,7 +41,7 @@ public class Apk {
     Timber.d("%s : Searching for package %s in app %s", currentThread, packageName, app.label());
 
     int index = Collections.binarySearch(sortedClassNames, packageName, (currentItem, key) -> {
-      if(currentItem.startsWith(key)) {
+      if (currentItem.startsWith(key)) {
         return 0;
       }
       return currentItem.compareTo(key);
