9a8faf04950846823c3afb0785eb222342e46227
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-03-16 15:47:13 +0100

    Prevent null pointer exception

--- app/src/main/java/de/philipphager/disclosure/util/device/FileUtils.java
+++ app/src/main/java/de/philipphager/disclosure/util/device/FileUtils.java
@@ -4,19 +4,27 @@ import java.io.File;
 import javax.inject.Inject;
 import timber.log.Timber;
 
+import static de.philipphager.disclosure.util.assertion.Assertions.ensureNotNull;
+
 public class FileUtils {
   @Inject @SuppressWarnings("PMD.UnnecessaryConstructor") public FileUtils() {
     // Needed for dagger injection.
   }
 
   public void delete(File file) {
-    if (file != null && file.isDirectory()) {
-      for (File c : file.listFiles()) {
-        delete(c);
+    ensureNotNull(file, "Attempt to delete null file");
+
+    if (file.isDirectory()) {
+      File[] files = file.listFiles();
+
+      if (files != null) {
+        for (File c : files) {
+          delete(c);
+        }
       }
     }
 
-    if (file != null && !file.delete()) {
+    if (!file.delete()) {
       Timber.e("failed to delete file %s", file);
     }
   }
