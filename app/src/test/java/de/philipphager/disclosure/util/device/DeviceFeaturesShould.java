c3ed29f6c12d59a2bfce1e77936706270972c187
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-03-19 23:53:08 +0100

    Replace magic numbers

--- app/src/test/java/de/philipphager/disclosure/util/device/DeviceFeaturesShould.java
+++ app/src/test/java/de/philipphager/disclosure/util/device/DeviceFeaturesShould.java
@@ -1,5 +1,6 @@
 package de.philipphager.disclosure.util.device;
 
+import android.os.Build;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -9,12 +10,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(MockitoJUnitRunner.class)
 public class DeviceFeaturesShould {
   @Test public void notSupportRuntimePermissionsBeforeAndroidMarshmallow() {
-    DeviceFeatures deviceFeatures = new DeviceFeatures(22);
+    DeviceFeatures deviceFeatures = new DeviceFeatures(Build.VERSION_CODES.LOLLIPOP_MR1);
     assertThat(deviceFeatures.supportsRuntimePermissions()).isFalse();
   }
 
   @Test public void supportRuntimePermissionsOnMarshmallowAndHigher() {
-    DeviceFeatures deviceFeatures = new DeviceFeatures(23);
+    DeviceFeatures deviceFeatures = new DeviceFeatures(Build.VERSION_CODES.M);
     assertThat(deviceFeatures.supportsRuntimePermissions()).isTrue();
   }
 }
