34541db95d271fd594b77430851e003e20b174f7
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-03-20 22:47:12 +0100

    Split Apk into Apk and ApkAnalyzer
    Better separation of concerns.

--- app/src/main/java/de/philipphager/disclosure/feature/analyser/app/Apk.java
+++ app/src/main/java/de/philipphager/disclosure/feature/analyser/app/Apk.java
@@ -8,19 +8,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import rx.Observable;
-import timber.log.Timber;
 
 public class Apk {
-  private static final int MIN_INDEX = 0;
   private final App app;
-  private List<String> sortedClassNames;
 
   public Apk(App app) throws IOException {
     this.app = app;
-    load();
   }
 
-  private void load() throws IOException {
+  public App getApp() {
+    return app;
+  }
+
+  public Observable<List<String>> getUsedClasses() throws IOException {
     File file = new File(app.sourceDir());
     List<String> classNames = new ArrayList<>();
 
@@ -29,23 +29,8 @@ public class Apk {
       classNames.addAll(Collections.list(dexFile.entries()));
     }
 
-    sortedClassNames = Observable.from(classNames)
+    return Observable.from(classNames)
         .distinct()
-        .toSortedList()
-        .toBlocking()
-        .first();
-  }
-
-  public boolean containsPackage(String packageName) {
-    String currentThread = Thread.currentThread().getName();
-    Timber.v("%s : Searching for package %s in app %s", currentThread, packageName, app.label());
-
-    int index = Collections.binarySearch(sortedClassNames, packageName, (currentItem, key) -> {
-      if (currentItem.startsWith(key)) {
-        return 0;
-      }
-      return currentItem.compareTo(key);
-    });
-    return index >= MIN_INDEX;
+        .toSortedList();
   }
 }
--- app/src/main/java/de/philipphager/disclosure/feature/analyser/library/usecase/AnalyseUsedLibraries.java
+++ app/src/main/java/de/philipphager/disclosure/feature/analyser/library/usecase/AnalyseUsedLibraries.java
@@ -3,6 +3,7 @@ package de.philipphager.disclosure.feature.analyser.library.usecase;
 import de.philipphager.disclosure.database.app.model.App;
 import de.philipphager.disclosure.database.library.model.Library;
 import de.philipphager.disclosure.feature.analyser.app.Apk;
+import de.philipphager.disclosure.feature.analyser.app.ApkAnalyzer;
 import de.philipphager.disclosure.service.LibraryService;
 import java.util.List;
 import javax.inject.Inject;
@@ -24,7 +25,7 @@ public class AnalyseUsedLibraries {
         .toBlocking()
         .first();
 
-    return loadApk(app)
+    return loadApkAnalyzer(app)
         .flatMap(this::findLibraries)
         .flatMap(Observable::from)
         .filter(library -> !knownLibraries.contains(library))
@@ -32,17 +33,20 @@ public class AnalyseUsedLibraries {
         .doOnNext(libraries -> libraryService.insertForApp(app, libraries));
   }
 
-  private Observable<Apk> loadApk(App app) {
-    return Observable.fromCallable(() -> new Apk(app));
+  private Observable<ApkAnalyzer> loadApkAnalyzer(App app) {
+    return Observable.fromCallable(() -> {
+      Apk apk = new Apk(app);
+      return new ApkAnalyzer(apk);
+    });
   }
 
-  private Observable<List<Library>> findLibraries(Apk apk) {
+  private Observable<List<Library>> findLibraries(ApkAnalyzer apkAnalyzer) {
     return libraryService.all()
         .first()
         .flatMap(Observable::from)
         .flatMap(library -> Observable.just(library)
             .subscribeOn(Schedulers.computation())
-            .filter(lib -> apk.containsPackage(lib.packageName()))
+            .filter(lib -> apkAnalyzer.usesLibrary(lib.packageName()))
         ).toList();
   }
 }
