acafa2c838642fd0819521f7275354831f80b989
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-03-22 00:23:39 +0100

    Clean up AppReport create method

--- app/src/main/java/de/philipphager/disclosure/database/app/model/AppReport.java
+++ app/src/main/java/de/philipphager/disclosure/database/app/model/AppReport.java
@@ -3,11 +3,12 @@ package de.philipphager.disclosure.database.app.model;
 import com.google.auto.value.AutoValue;
 
 @AutoValue public abstract class AppReport implements AppModel.ReportModel {
-  public static final AppModel.ReportMapper MAPPER = new AppModel.ReportMapper((App, libraryCount, permissionCount) -> create(App, libraryCount, permissionCount), App.FACTORY);
+  public static final AppModel.ReportMapper MAPPER = new AppModel.ReportMapper(
+      (app, libraryCount, permissionCount) -> create((App) app, libraryCount, permissionCount),
+      App.FACTORY);
 
-  public static AppReport create(AppModel app, long libraryCount, long permissionCount) {
-    //TODO: Fix whe
-    return new AutoValue_AppReport((App) app, libraryCount, permissionCount);
+  public static AppReport create(App app, long libraryCount, long permissionCount) {
+    return new AutoValue_AppReport(app, libraryCount, permissionCount);
   }
 
   /**
